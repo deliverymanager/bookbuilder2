523c4268d1f101465b3f9e07871cc34cfcf53651
deliverymanager <info@deliverymanager.gr>
2018-02-21 12:20:46 +0200

    Anestis

--- build_config.js
+++ build_config.js
@@ -229,7 +229,7 @@ var buildAndroid = function (versionForVersionCode, minSdkVersion, generalCallba
     async.waterfall([
       function (callback) {
 
-        exec("cd " + groupDirectory + "; ionic cordova platform save; ionic doctor check; ionic cordova platform add android@" + androidVersion + " --save;  ionic cordova resources android --icon --force; ionic cordova resources android --splash --force;", {maxBuffer: 20000000000}, function (error, stdout, stderr) {
+        exec("cd " + groupDirectory + "; ionic doctor check; ionic cordova platform add android@" + androidVersion + " --save;  ionic cordova resources android --icon --force; ionic cordova resources android --splash --force;", {maxBuffer: 20000000000}, function (error, stdout, stderr) {
 
           if (error) {
             console.log(error);
@@ -246,7 +246,7 @@ var buildAndroid = function (versionForVersionCode, minSdkVersion, generalCallba
       },
       function (callback) {
 
-        exec("cd " + groupDirectory + "; node hooks/scripts/add_plugins.js; ionic cordova prepare android -- --browserify; cordova plugins; ionic cordova build android -- --browserify;", {maxBuffer: 20000000000}, function (error, stdout, stderr) {
+        exec("cd " + groupDirectory + "; node hooks/scripts/add_plugins.js; ionic cordova prepare android -- --browserify; cordova plugins; ionic cordova build android -- --browserify; ionic cordova platform save;", {maxBuffer: 20000000000}, function (error, stdout, stderr) {
 
           if (error) {
             console.log(error);
